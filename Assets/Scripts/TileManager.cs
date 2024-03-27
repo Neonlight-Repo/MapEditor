21cb1ca4b7f694481b02bd269fb178011c67f88e
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2024-03-27 17:10:24 +0900

    일괄 변환 구현

---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    public List<MapFileConvertor> ignore;
+
+    [ContextMenu("ÀÏ°ý º¯È¯")]
+    public void ConvertAllMap()
+    {
+        MapFileConvertor[] maps = GetComponentsInChildren<MapFileConvertor>(true);
+        foreach(MapFileConvertor map in maps)
+        {
+            if (!ignore.Contains(map))
+            {
+                map.GenerateFile();
+            }
+        }
+    }
+}
